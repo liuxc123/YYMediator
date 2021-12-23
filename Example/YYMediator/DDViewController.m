5a67b5a0c078422e6ccebbeb039c535224473ac9
liuxc123 <lxc_work@126.com>
2021-12-23 17:27:23 +0800

    first commit

--- Example/YYMediator/DDViewController.m
+++ Example/YYMediator/DDViewController.m
@@ -38,37 +38,6 @@
     }];
 //    NSLog(@"%@", [[YYMediator sharedInstance] canOpenURL:@"app://showalert/controller/1"] ? @"YES" : @"NO");
     NSLog(@"%@", [[YYMediator sharedInstance] generateURLWithPattern:@"app://showalert/controller/:id/:detail_id" parameters:@[@1, @2]]);
-    
-//    UIViewController *vc = [[YYMediator sharedInstance] YYMediator_TargetAViewController:@{ @"title": @"custom title" }];
-//    [[YYMediator sharedInstance] pushViewController:vc animated:YES];
-    
-//    for (int i = 0; i < 10; i++) {
-//        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)((0.5 * i) * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//            if (i == 3) {
-//                DDViewController2 *vc = [DDViewController2 new];
-//                vc.view.backgroundColor = [UIColor yellowColor];
-//                [YYMediator.sharedInstance presentViewController:vc animated:YES completion:nil];
-//            } if (i % 2) {
-//                UIViewController *vc = [UIViewController new];
-//                vc.title = [NSString stringWithFormat:@"页面%d", i];
-//                vc.view.backgroundColor = [UIColor redColor];
-//                [YYMediator.sharedInstance presentViewController:vc animated:YES completion:nil];
-//            } else {
-//                [YYMediator.sharedInstance YYMediator_showAlertWithMessage:@"message" cancelAction:^(NSDictionary * _Nonnull info) {
-//
-//                } confirmAction:^(NSDictionary * _Nonnull info) {
-//
-//                }];
-//            }
-//        });
-//
-//    }
-//
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        [[YYMediator sharedInstance] moveToViewControllerClass:@"DDViewController" completion:^(UIViewController * vc) {
-//            NSLog(@"%@", NSStringFromClass([vc class]));
-//        }];
-//    });
 }
 
 @end
